ca17a155a5c33f031b759317ee2f21ff755cd218
Max Edmonds <maxedmonds05@gmail.com>
2020-06-13 18:44:45 -0700

    add starter HTML CSS

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+function RenderSliderPreferences() {
+    document.getElementById("applySliderData").innerHTML = "Hello World";
+}
+
+
+{/* <input id="applySliderData" onclick=RenderSliderPreferences() class="button u-full-width" type = "submit" value = "Apply" /> */ }
