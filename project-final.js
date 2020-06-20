2fe0a86698cc6ce8614e3a13c9d605b6f4d33bea
KaiTaylorC <kileytaylorc@gmail.com>
2020-06-20 08:14:28 -0700

    final touches to the weather API javascript

--- project-final.js
+++ project-final.js
@@ -1,6 +1,7 @@
 const appId = 'b8cce7d514214016b192b5bfa4958cbf';
 let units = 'imperial';
 var hello = null
+var attrArray = []
 
 function getSearchMethod(searchTerm) {
     if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
@@ -17,7 +18,9 @@ function searchWeather(searchTerm) {
     .then(result => {
         console.log('result from api is: ', result);
         init(result);
-        console.log(hello)
+		console.log(hello)
+		// weatherGeneratorCall(result);
+		// console.log('test')
     })
 }
 function init(resultFromServer) {
@@ -26,77 +29,110 @@ function init(resultFromServer) {
 }
 
 function weatherGeneratorCall(resultFromServer) {
-    switch (resultFromServer.weather.decription) {
-    	case 'Clear':
+	switch (resultFromServer.data[0].weather.description) {
+		case 'Clear sky':
 		case 'Smoke':
 		case 'Sand/Dust':
-			danceMax = 1
-			danceMin = .3
-			accMax = .5
-			accMin = 0 //accoustic
-			enMax = 1 //energy
-			enMin = .5
-			instMax = .6
-            instMin = .1 //instrumental
-            valMax = 1
-            valMin = .4
-        	break;
-		case 'Clouds':
-			danceMax = .7
-			danceMin = .2
-			accMax = 1
-			accMin = .2 //accoustic
-			enMax = .7 //energy
-			enMin = 0
-			instMax = 1
-            instMin = .3 //instrumental
-            valMax = .8
-            valMin = .3
+		case 'Unkown Precipitation':
+		case 'Few clouds':
+			attrArray[0] = 1
+			attrArray[1] = .3
+			attrArray[2] = .5
+			attrArray[3] = 0
+			attrArray[4] = 1 
+			attrArray[5] = .5
+			attrArray[6] = .6
+			attrArray[7] = .1
+			attrArray[8] = 1
+			attrArray[9] = .4
+			console.log("In the function "+ attrArray)
 			break;
-		case 'Rain':
-        case 'Drizzle':
-        case 'Mist':
+		case 'Scattered clouds':
+		case 'Broken clouds':
+		case 'Overcast clouds':
+			attrArray[0] = .7
+			attrArray[1] = .2
+			attrArray[2] = 1
+			attrArray[3] = .2
+			attrArray[4] = .7
+			attrArray[5] = 0
+			attrArray[6] = 1
+			attrArray[7] = .3
+			attrArray[8] = .8
+			attrArray[9] = .3
+			console.log("In the function "+ attrArray)
+			break;
+		case 'Light Rain':
+		case 'Moderate Rain':
+		case 'Heavy Rain':
+		case 'Drizzle':
+		case 'Heavy Drizzle':
+		case 'Light shower rain':
+		case 'Shower rain':
+		case 'Heavy shower rain':
+		case 'Mist':
 		case 'Fog':
-			danceMax = .6
-			danceMin = 0
-			accMax = 1
-			accMin = .2 //accoustic
-			enMax = .6 //energy
-			enMin = 0
-			instMax = 1
-            instMin = .3 //instrumental
-            valMax = .7
-            valMin = 0
-            break;
-        case 'Thunderstorm':
-            danceMax = .6
-			danceMin = 0
-			accMax = 1
-			accMin = .2 //accoustic
-			enMax = .6 //energy
-			enMin = 0
-			instMax = 1
-            instMin = .3 //instrumental
-            valMax = .7
-            valMin = 0
-            break;
-        case 'Snow':
+		case 'Freezing Fog':
+		case 'Haze':
+			attrArray[0] = .6
+			attrArray[1] = 0
+			attrArray[2] = 1
+			attrArray[3] = .2
+			attrArray[4] = .6
+			attrArray[5] = 0
+			attrArray[6] = 1
+			attrArray[7] = .3
+			attrArray[8] = .7
+			attrArray[9] = 0
+			console.log("In the function "+ attrArray)
+			break;
+		case 'Thunderstorm with light rain':
+		case 'Thunderstorn with rain':
+		case 'Thunderstorn with heavy rain':
+		case 'Thunderstorm with light drizzle':
+		case 'Thunderstorm with drizzle':
+		case 'Thunderstorm with heavy drizzle':
+		case 'Thunderstorm with hail':
+			attrArray[0] = .6
+			attrArray[1] = 0
+			attrArray[2] = 1
+			attrArray[3] = .2 
+			attrArray[4] = .6
+			attrArray[5] = 0
+			attrArray[6] = 1
+			attrArray[7] = .3
+			attrArray[8] = .7
+			attrArray[9] = 0
+			console.log("In the function "+ attrArray)
+			break;
+		case 'Light snow':
+		case 'Snow':
+		case 'Heavy snow':
+		case 'Mix snow/rain':
+		case 'Sleet':
+		case 'Heavy sleet':
+		case 'Snow shower':
+		case 'Heavy snow shower':
 		case 'Flurries':
-            danceMax = .8
-			danceMin = .3
-			accMax = 1
-			accMin = .2 //accoustic
-			enMax = .8 //energy
-			enMin = .2
-			instMax = .8
-            instMin = .2 //instrumental
-            valMax = 1
-            valMin = .3
-            break;
-
-        default:
-        	break;
- 	}
+		case 'Freezing rain':
+			attrArray[0] = .8
+			attrArray[1] = .3
+			attrArray[2] = 1
+			attrArray[3] = .2
+			attrArray[4] = .8
+			attrArray[5] = .2
+			attrArray[6] = .8
+			attrArray[7] = .2
+			attrArray[8] = 1
+			attrArray[9] = .3
+			console.log("In the function "+ attrArray)
+			break;
+		default:
+			console.log('dam')
+			break;
+	}
+	console.log(attrArray)
+	return attrArray
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
